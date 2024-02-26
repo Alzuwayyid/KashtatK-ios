ba27d34072958af394ccc73d335ce8bb6b80472b
Alzuwayyid <bo-hajr-a@hotmail.com>
2024-02-26 15:14:27 +0300

    Creating the project

---
+++ KashtatK/KashtatKApp.swift
@@ -0,0 +1,17 @@
+//
+//  KashtatKApp.swift
+//  KashtatK
+//
+//  Created by Mohammed on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct KashtatKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
